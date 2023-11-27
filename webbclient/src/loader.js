5d9a7ffbead6f6becc1545c621eb0fad98635de5
Julia Lind <juli22@student.bth.se>
2023-11-27 11:59:30 +0100

    removed a couple of bugs caught by Scrutinizer

--- webbclient/src/loader.js
+++ webbclient/src/loader.js
@@ -10,7 +10,7 @@ const loader = {
     show: function show() {
         const check = document.querySelector(".loader");
 
-        if (check == null) {
+        if (check === null) {
             const myLoader = createElement("div.loader");
 
             document.body.appendChild(myLoader);
@@ -23,7 +23,7 @@ const loader = {
     hide: function hide() {
         const myLoader = document.querySelector(".loader");
 
-        if (myLoader != null) {
+        if (myLoader !== null) {
             myLoader.classList.add("loader--hidden");
             myLoader.addEventListener("transitionend", () => {
                 myLoader.remove();
